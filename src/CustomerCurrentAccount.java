cda267b6a329bab7b43b5c6f32a3fe02b29b0ae4
Conor Shields <42182485+cshields236@users.noreply.github.com>
2020-03-06 01:09:44 +0000

    moved withdraw fucntionality to the CustomerCurrentAccount class

--- src/CustomerCurrentAccount.java
+++ src/CustomerCurrentAccount.java
@@ -1,4 +1,6 @@
+import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class CustomerCurrentAccount extends CustomerAccount
 {
@@ -27,4 +29,82 @@ public class CustomerCurrentAccount extends CustomerAccount
         this.atm = atm;
     }
 
+    public void withdraw(CustomerAccount acc, Customer e,JFrame f) {
+        boolean loop = true;
+        boolean on = true;
+        double withdraw = 0;
+        Menu m = new Menu();
+        if (acc instanceof CustomerCurrentAccount) {
+            int count = 3;
+            int checkPin = ((CustomerCurrentAccount) acc).getAtm().getPin();
+            loop = true;
+
+            while (loop) {
+                if (count == 0) {
+                    JOptionPane.showMessageDialog(f, "Pin entered incorrectly 3 times. ATM card locked.", "Pin", JOptionPane.INFORMATION_MESSAGE);
+                    ((CustomerCurrentAccount) acc).getAtm().setValid(false);
+                    m.customer(e);
+                    loop = false;
+                    on = false;
+                }
+
+                String Pin = JOptionPane.showInputDialog(f, "Enter 4 digit PIN;");
+                int i = Integer.parseInt(Pin);
+
+                if (on) {
+                    if (checkPin == i) {
+                        loop = false;
+                        JOptionPane.showMessageDialog(f, "Pin entry successful", "Pin", JOptionPane.INFORMATION_MESSAGE);
+
+                    } else {
+                        count--;
+                        JOptionPane.showMessageDialog(f, "Incorrect pin. " + count + " attempts remaining.", "Pin", JOptionPane.INFORMATION_MESSAGE);
+
+                    }
+
+                }
+            }
+
+
+        }
+        if (on == true) {
+            String balanceTest = JOptionPane.showInputDialog(f, "Enter amount you wish to withdraw (max 500):");//the isNumeric method tests to see if the string entered was numeric.
+            if (m.isNumeric(balanceTest)) {
+
+                withdraw = Double.parseDouble(balanceTest);
+                loop = false;
+
+
+            } else {
+                JOptionPane.showMessageDialog(f, "You must enter a numerical value!", "Oops!", JOptionPane.INFORMATION_MESSAGE);
+            }
+            if (withdraw > 500) {
+                JOptionPane.showMessageDialog(f, "500 is the maximum you can withdraw at a time.", "Oops!", JOptionPane.INFORMATION_MESSAGE);
+                withdraw = 0;
+            }
+            if (withdraw > acc.getBalance()) {
+                JOptionPane.showMessageDialog(f, "Insufficient funds.", "Oops!", JOptionPane.INFORMATION_MESSAGE);
+                withdraw = 0;
+            }
+
+            String euro = "\u20ac";
+            acc.setBalance(acc.getBalance() - withdraw);
+            //recording transaction:
+            //		String date = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+            Date date = new Date();
+            String date2 = date.toString();
+
+            String type = "Withdraw";
+            double amount = withdraw;
+
+
+            AccountTransaction transaction = new AccountTransaction(date2, type, amount);
+            acc.getTransactionList().add(transaction);
+
+
+            JOptionPane.showMessageDialog(f, withdraw + euro + " withdrawn.", "Withdraw", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(f, "New balance = " + acc.getBalance() + euro, "Withdraw", JOptionPane.INFORMATION_MESSAGE);
+        }
+
+    }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1004,83 +1004,8 @@ public class Menu extends JFrame {
 
                     withdrawButton.addActionListener(new ActionListener() {
                         public void actionPerformed(ActionEvent ae) {
-                            boolean loop = true;
-                            boolean on = true;
-                            double withdraw = 0;
-
-                            if (acc instanceof CustomerCurrentAccount) {
-                                int count = 3;
-                                int checkPin = ((CustomerCurrentAccount) acc).getAtm().getPin();
-                                loop = true;
-
-                                while (loop) {
-                                    if (count == 0) {
-                                        JOptionPane.showMessageDialog(f, "Pin entered incorrectly 3 times. ATM card locked.", "Pin", JOptionPane.INFORMATION_MESSAGE);
-                                        ((CustomerCurrentAccount) acc).getAtm().setValid(false);
-                                        customer(c);
-                                        loop = false;
-                                        on = false;
-                                    }
-
-                                    String Pin = JOptionPane.showInputDialog(f, "Enter 4 digit PIN;");
-                                    int i = Integer.parseInt(Pin);
-
-                                    if (on) {
-                                        if (checkPin == i) {
-                                            loop = false;
-                                            JOptionPane.showMessageDialog(f, "Pin entry successful", "Pin", JOptionPane.INFORMATION_MESSAGE);
-
-                                        } else {
-                                            count--;
-                                            JOptionPane.showMessageDialog(f, "Incorrect pin. " + count + " attempts remaining.", "Pin", JOptionPane.INFORMATION_MESSAGE);
-
-                                        }
-
-                                    }
-                                }
-
-
-                            }
-                            if (on == true) {
-                                String balanceTest = JOptionPane.showInputDialog(f, "Enter amount you wish to withdraw (max 500):");//the isNumeric method tests to see if the string entered was numeric.
-                                if (isNumeric(balanceTest)) {
-
-                                    withdraw = Double.parseDouble(balanceTest);
-                                    loop = false;
-
-
-                                } else {
-                                    JOptionPane.showMessageDialog(f, "You must enter a numerical value!", "Oops!", JOptionPane.INFORMATION_MESSAGE);
-                                }
-                                if (withdraw > 500) {
-                                    JOptionPane.showMessageDialog(f, "500 is the maximum you can withdraw at a time.", "Oops!", JOptionPane.INFORMATION_MESSAGE);
-                                    withdraw = 0;
-                                }
-                                if (withdraw > acc.getBalance()) {
-                                    JOptionPane.showMessageDialog(f, "Insufficient funds.", "Oops!", JOptionPane.INFORMATION_MESSAGE);
-                                    withdraw = 0;
-                                }
-
-                                String euro = "\u20ac";
-                                acc.setBalance(acc.getBalance() - withdraw);
-                                //recording transaction:
-                                //		String date = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
-                                Date date = new Date();
-                                String date2 = date.toString();
-
-                                String type = "Withdraw";
-                                double amount = withdraw;
-
-
-                                AccountTransaction transaction = new AccountTransaction(date2, type, amount);
-                                acc.getTransactionList().add(transaction);
-
-
-                                JOptionPane.showMessageDialog(f, withdraw + euro + " withdrawn.", "Withdraw", JOptionPane.INFORMATION_MESSAGE);
-                                JOptionPane.showMessageDialog(f, "New balance = " + acc.getBalance() + euro, "Withdraw", JOptionPane.INFORMATION_MESSAGE);
-                            }
-
-
+                            CustomerCurrentAccount currentAccount = new CustomerCurrentAccount();
+                            currentAccount.withdraw(acc, finalE, f);
                         }
                     });
 
