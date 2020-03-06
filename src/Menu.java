7f4d4936dd8f474216ba260d33799ac29a1e3371
Conor Shields <42182485+cshields236@users.noreply.github.com>
2020-03-06 01:04:19 +0000

    moved lodge functionality from the menu class to the Customer Deposit Account class

--- src/Menu.java
+++ src/Menu.java
@@ -892,6 +892,7 @@ public class Menu extends JFrame {
                 }
             });
 
+            Customer finalE = e1;
             continueButton.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent ae) {
 
@@ -994,70 +995,9 @@ public class Menu extends JFrame {
 
                     lodgementButton.addActionListener(new ActionListener() {
                         public void actionPerformed(ActionEvent ae) {
-                            boolean loop = true;
-                            boolean on = true;
-                            double balance = 0;
+                            CustomerDepositAccount depositAccount = new CustomerDepositAccount();
+                            depositAccount.lodge(acc, finalE, f);
 
-                            if (acc instanceof CustomerCurrentAccount) {
-                                int count = 3;
-                                int checkPin = ((CustomerCurrentAccount) acc).getAtm().getPin();
-                                loop = true;
-
-                                while (loop) {
-                                    if (count == 0) {
-                                        JOptionPane.showMessageDialog(f, "Pin entered incorrectly 3 times. ATM card locked.", "Pin", JOptionPane.INFORMATION_MESSAGE);
-                                        ((CustomerCurrentAccount) acc).getAtm().setValid(false);
-                                        customer(c);
-                                        loop = false;
-                                        on = false;
-                                    }
-
-                                    String Pin = JOptionPane.showInputDialog(f, "Enter 4 digit PIN;");
-                                    int i = Integer.parseInt(Pin);
-
-                                    if (on) {
-                                        if (checkPin == i) {
-                                            loop = false;
-                                            JOptionPane.showMessageDialog(f, "Pin entry successful", "Pin", JOptionPane.INFORMATION_MESSAGE);
-
-                                        } else {
-                                            count--;
-                                            JOptionPane.showMessageDialog(f, "Incorrect pin. " + count + " attempts remaining.", "Pin", JOptionPane.INFORMATION_MESSAGE);
-                                        }
-
-                                    }
-                                }
-
-
-                            }
-                            if (on == true) {
-                                String balanceTest = JOptionPane.showInputDialog(f, "Enter amount you wish to lodge:");//the isNumeric method tests to see if the string entered was numeric.
-                                if (isNumeric(balanceTest)) {
-
-                                    balance = Double.parseDouble(balanceTest);
-                                    loop = false;
-
-
-                                } else {
-                                    JOptionPane.showMessageDialog(f, "You must enter a numerical value!", "Oops!", JOptionPane.INFORMATION_MESSAGE);
-                                }
-
-
-                                String euro = "\u20ac";
-                                acc.setBalance(acc.getBalance() + balance);
-                                // String date = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
-                                Date date = new Date();
-                                String date2 = date.toString();
-                                String type = "Lodgement";
-                                double amount = balance;
-
-
-                                AccountTransaction transaction = new AccountTransaction(date2, type, amount);
-                                acc.getTransactionList().add(transaction);
-
-                                JOptionPane.showMessageDialog(f, balance + euro + " added do you account!", "Lodgement", JOptionPane.INFORMATION_MESSAGE);
-                                JOptionPane.showMessageDialog(f, "New balance = " + acc.getBalance() + euro, "Lodgement", JOptionPane.INFORMATION_MESSAGE);
-                            }
 
                         }
                     });
